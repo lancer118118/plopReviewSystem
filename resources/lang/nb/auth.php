57eb92c69c34ce718a3c916a86ad433fc24e0721
lancer118118 <piao124@outloook.com>
2022-01-26 07:14:07 +0200

    update init
    update init

---
+++ resources/lang/nb/auth.php
@@ -0,0 +1,27 @@
+<?php 
+
+return [
+    'failed' => 'Disse legitimasjonene samsvarer ikke med opplysningene våre.',
+    'throttle' => 'For mange påloggingsforsøk. Vennligst prøv igjen i :seconds seconds.',
+    'login' => 'Logg Inn',
+    'register' => 'Opprette ny konto',
+    'account_info' => 'Kontoinformasjon',
+    'change_info' => 'Endre info',
+    'change_password' => 'Bytt passord',
+    'logout' => 'Logg ut',
+    'remember_me' => 'Hold meg innlogget',
+    'information_updated' => 'Informasjonen din er oppdatert.',
+    'name' => 'Navn',
+    'email' => 'E-post',
+    'password' => 'Passord',
+    'confirm_password' => 'Bekreft passord',
+    'old_password' => 'Gammelt passord',
+    'new_password' => 'Nytt passord',
+    'cancel' => 'Avbryt',
+    'reset_password' => 'Tilbakestille passord',
+    'send_password_reset_link' => 'Send lenke for tilbakestilling av passord',
+    'forgot_password_question' => 'Glemt passord?',
+    'wrong_password' => 'Det gamle passordet du skrev inn er feil.',
+    'password_updated' => 'Ditt passord har blitt endret.',
+    'registration_closed' => 'Påmeldingen er stengt.',
+];
